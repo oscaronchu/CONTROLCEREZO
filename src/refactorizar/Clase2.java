ec7dce59f9ce0882d77b5a4024cee0675cc926ad
dam <dam@203P03>
2022-02-04 11:47:33 +0100

    PARTE 7 EXAMEN

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    int c;
+    int d;
+}
